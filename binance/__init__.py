2af06f4bb206e46c1a9167961698dbc593fe0a71
marianoeramirez <Sosinformatico1990>
2022-11-24 09:44:23 -0400

    module basic information

--- binance/__init__.py
+++ binance/__init__.py
@@ -1,10 +1,13 @@
 """An unofficial Python wrapper for the Binance exchange API v3
 
-.. moduleauthor:: Sam McHardy
+The original author is Sam McHardy, (https://github.com/sammchardy/python-binance)
+I just take his work to improve and maintain.
+
+.. moduleauthor:: Mariano Ramirez
 
 """
 
-__version__ = '1.0.16'
+__version__ = '1.0.0'
 
 from binance.client import Client, AsyncClient  # noqa
 from binance.depthcache import DepthCacheManager, OptionsDepthCacheManager  # noqa
