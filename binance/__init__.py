c545489ed0e6a1d6ffcf3de9b8a86010ffefd04a
marianoeramirez <Sosinformatico1990>
2022-11-23 21:22:27 -0400

    remove ThreadedWebsocketManager

--- binance/__init__.py
+++ binance/__init__.py
@@ -7,5 +7,5 @@
 __version__ = '1.0.16'
 
 from binance.client import Client, AsyncClient  # noqa
-from binance.depthcache import DepthCacheManager, OptionsDepthCacheManager, ThreadedDepthCacheManager  # noqa
-from binance.streams import BinanceSocketManager, ThreadedWebsocketManager  # noqa
+from binance.depthcache import DepthCacheManager, OptionsDepthCacheManager  # noqa
+from binance.streams import BinanceSocketManager  # noqa
